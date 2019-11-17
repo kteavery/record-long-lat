c651986743ac6246280f851e8f89f54a677837ed
Kate Avery <kteavery@gmail.com>
2019-11-16 18:04:04 -0600

    add flag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { fs } from './system/fs'
 
+const path = require('path')
+
+let flag_bool = false
+
 fs.writeFile('output.csv', 'x,y\n', err => {
   if (err) throw err
 })
@@ -13,23 +17,43 @@ prev.onclick = prev_refresh
 let clear = document.getElementById('clear-button')
 clear.onclick = clear_marks
 
+let flag = document.getElementById('flag-button')
+flag.onclick = flag_image
+
 var pics = []
 let i = -1
 
-fields = []
-fs.readdir(
-  '/Users/Kate/workspace/record-long-lat/data/Roost_Reflectivity',
-).then(result => fields.append(result))
-fs.readdir('/Users/Kate/workspace/record-long-lat/data/Roost_Velocity').then(
-  result => fields.append(result),
+let aws_names = []
+fs.readFile('/Users/Kate/workspace/record-long-lat/aws_names.csv').then(
+  result => aws_names.push(result),
 )
-fs.readdir('/Users/Kate/workspace/record-long-lat/data/Roost_Rho_HV').then(
-  result => fields.append(result),
-)
-fs.readdir('/Users/Kate/workspace/record-long-lat/data/Roost_Zdr').then(
-  result => fields.append(result),
-)
-pics.append(fields)
+console.log('AWS')
+console.log(aws_names)
+
+for (let k = 0; k < aws_names.length; k++) {
+  let fields = []
+  fs.readFile(
+    '/Users/Kate/workspace/record-long-lat/data/Roost_Reflectivity/' +
+      aws_names[k] +
+      '_Reflectivity.png',
+  ).then(result => fields.push(result))
+  fs.readFile(
+    '/Users/Kate/workspace/record-long-lat/data/Roost_Velocity/' +
+      aws_names[k] +
+      '_Velocity.png',
+  ).then(result => fields.push(result))
+  fs.readFile(
+    '/Users/Kate/workspace/record-long-lat/data/Roost_Rho_HV/' +
+      aws_names[k] +
+      '_Rho_HV',
+  ).then(result => fields.push(result))
+  fs.readFile(
+    '/Users/Kate/workspace/record-long-lat/data/Roost_Zdr/' +
+      aws_names[k] +
+      '_Zdr',
+  ).then(result => fields.push(result))
+  pics.push(fields)
+}
 
 // var pics = [
 //   [
@@ -45,6 +69,16 @@ pics.append(fields)
 //     "url('data/01KAKQ20150801_101421_V06_Rho_HV.png')",
 //   ],
 // ]
+
+function read_directories() {}
+
+function flag_image() {
+  if (flag_bool == false) {
+    flag_bool = true
+  } else {
+    flag_bool = false
+  }
+}
 
 function next_refresh() {
   if (i < pics.length) {
@@ -64,24 +98,17 @@ function prev_refresh() {
   refresh_page(pics[i])
 }
 
-// function get_directories(path, callback) {
-//   fs.readdir(path, function(content) {
-//     callback(content)
-//   })
-// }
-
 function mark(event) {
   //get the position
   let pos_x = event.offsetX ? event.offsetX : event.pageX
   let pos_y = event.offsetY ? event.offsetY : event.pageY
 
   //create and add a new marker
-  let marker = document.createElement('p')
+  let marker = document.createElement('li')
   marker.setAttribute('class', 'marker')
-  let text = document.createTextNode(pos_x + ', ' + pos_y)
+  let text = document.createTextNode(pos_x + ', ' + pos_y + ', ' + flag_bool)
   marker.appendChild(text)
-
-  document.pointform.append(marker)
+  document.getElementById('mark-list').appendChild(marker)
 
   // write to csv
   let data = [pos_x, pos_y]
@@ -91,7 +118,7 @@ function mark(event) {
   })
   console.log(data)
 
-  return [event.pageX, event.pageY]
+  return [pos_x, pos_y]
 }
 
 function clear_marks() {
@@ -120,12 +147,11 @@ function generate_images(imgs) {
   if (imgs != undefined) {
     for (let j = 0; j < imgs.length; j++) {
       // and create a new image on the page for each one
-      let image = document.createElement('div')
+      let image = document.createElement('img')
       image.setAttribute('class', 'image')
-      //image.setAttribute('onclick', 'mark(event)')
       image.onclick = mark
-      image.style.backgroundImage = imgs[j]
-      //console.log(imgs[i])
+      image.src = imgs[j]
+      //image.style.backgroundImage = imgs[j]
       image.style.backgroundRepeat = 'no-repeat'
       image.style.width = '250px'
       image.style.height = '250px'
